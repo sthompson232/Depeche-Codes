5a381942fb2da0a3d222cd5e772904a344ff600e
Sam Thompson <samthompson292@gmail.com>
2020-12-07 14:45:54 +0000

    added custom images for each project

--- helper.py
+++ helper.py
@@ -1,18 +1,66 @@
-names = {"eisenhowersquadrant": "Eisenhower's Quadrant", "depechehouse": "Depeche House", "depechecodes": "Depeche Codes", "mileagecalculator": "Mileage Calculator"}
+names = {
+    "eisenhowersquadrant": "Eisenhower's Quadrant", 
+    "depechehouse": "Depeche House", 
+    "depechecodes": "Depeche Codes", 
+    "mileagecalculator": "Mileage Calculator"
+    }
 
-descriptions = {"eisenhowersquadrant": "Using the Eisenhower's Quadrant concept, this project allows you to organise different tasks. You are able to add, delete, and complete tasks. You are also able to see information about different tasks, for instance when they were created and when they must be finished by. This project showcases my Python coding skills in back-end web development.", "depechehouse": "A social app which allows users to create accounts using email, password and usernames. Users are able to post messages to a global feed which can be seen by all other users. Users can edit or delete their posts, change their profile picture, uesrname and password. This projects showcases advanced flask functions.", "depechecodes": "This is my portfolio website, designed to feature all of my past projects. It has been designed to easily navigate through my projects with a filter function to search through projects via their related tags. This project showcases my ability with front-end design, and my knowledge of HTML and CSS.", "mileagecalculator": "This was the first project I made. This used pure Python and could only be used in a CLI. Enter your current car mileage into the terminal and the calculator will update your CSV file with different information."}
+descriptions = {
+    "eisenhowersquadrant": "Using the Eisenhower's Quadrant concept, this project allows you to organise different tasks. You are able to add, delete, and complete tasks. You are also able to see information about different tasks, for instance when they were created and when they must be finished by. This project showcases my Python coding skills in back-end web development.", 
+    "depechehouse": "A social app which allows users to create accounts using email, password and usernames. Users are able to post messages to a global feed which can be seen by all other users. Users can edit or delete their posts, change their profile picture, uesrname and password. This projects showcases advanced flask functions.", 
+    "depechecodes": "This is my portfolio website, designed to feature all of my past projects. It has been designed to easily navigate through my projects with a filter function to search through projects via their related tags. This project showcases my ability with front-end design, and my knowledge of HTML and CSS.", 
+    "mileagecalculator": "This was the first project I made. This used pure Python and could only be used in a CLI. Enter your current car mileage into the terminal and the calculator will update your CSV file with different information."
+    }
 
-brief_desc = {"eisenhowersquadrant": "Task management app using Flask and CSV as a database", "depechehouse": "An in depth Flask app using SQL and authentication. Allows users to share posts with each other", "depechecodes": "My portfolio website built on Flask and deployed on Heroku", "mileagecalculator": "My first project, Python app used in the terminal to perform useful calculations for car mileage."}
+brief_desc = {
+    "eisenhowersquadrant": "Task management app using Flask and CSV as a database", 
+    "depechehouse": "An in depth Flask app using SQL and authentication. Allows users to share posts with each other", 
+    "depechecodes": "My portfolio website built on Flask and deployed on Heroku", 
+    "mileagecalculator": "My first project, Python app used in the terminal to perform useful calculations for car mileage."
+    }
 
-dates = {"eisenhowersquadrant": "17/11/2020", "depechehouse": "29/11/2020", "depechecodes": "29/11/2020", "mileagecalculator": "27/10/2020"}
+dates = {
+    "eisenhowersquadrant": "17/11/2020", 
+    "depechehouse": "29/11/2020", 
+    "depechecodes": "29/11/2020", 
+    "mileagecalculator": "27/10/2020"
+    }
 
-tags = {"eisenhowersquadrant": "Python, Flask, CSV Files", "depechehouse": "Python, Flask, SQL", "depechecodes": "Python, Flask", "mileagecalculator": "Python, CSV Files"}
+tags = {
+    "eisenhowersquadrant": "Python, Flask, CSV Files", 
+    "depechehouse": "Python, Flask, SQL", 
+    "depechecodes": "Python, Flask", 
+    "mileagecalculator": "Python, CSV Files"
+    }
 
-goals = {"eisenhowersquadrant": ["To create my first flask app", "Learn how to read and write to CSV files before I learn SQL"], "depechehouse": ["Create a flask app with an SQL database", "Add authentication to the website", "Use the package format for the flask app", "Users can create accounts with different permissions."], "depechecodes":["A simple flask app to feature my work", "Learn basic front-end techniques using bootstrap"], "mileagecalculator": ["Putting my knowledge of Python into practice for the first time", "Learning to use the terminal", "Reading and writing to CSV"]}
+goals = {
+    "eisenhowersquadrant": ["To create my first flask app", "Learn how to read and write to CSV files before I learn SQL"], 
+    "depechehouse": ["Create a flask app with an SQL database", "Add authentication to the website", "Use the package format for the flask app", "Users can create accounts with different permissions."], 
+    "depechecodes":["A simple flask app to feature my work", "Learn basic front-end techniques using bootstrap"], 
+    "mileagecalculator": ["Putting my knowledge of Python into practice for the first time", "Learning to use the terminal", "Reading and writing to CSV"]
+    }
 
-github_links = {"eisenhowersquadrant": "https://github.com/Sam-Ian/Eisenhowers-Quadrant/", "depechehouse": "https://github.com/Sam-Ian/Depeche-House/", "depechecodes": "https://github.com/Sam-Ian/Depeche-Codes", "mileagecalculator": "https://github.com/Sam-Ian/Tesla-Mileage-Calculator"}
+github_links = {
+    "eisenhowersquadrant": "https://github.com/Sam-Ian/Eisenhowers-Quadrant/", 
+    "depechehouse": "https://github.com/Sam-Ian/Depeche-House/", 
+    "depechecodes": "https://github.com/Sam-Ian/Depeche-Codes", 
+    "mileagecalculator": "https://github.com/Sam-Ian/Tesla-Mileage-Calculator"
+    }
 
-project_links = {"eisenhowersquadrant": "https://eisenhowersquadrant.herokuapp.com/", "depechehouse": "https://depechehouse.herokuapp.com/", "depechecodes": "", "mileagecalculator": ""}
+project_links = {
+    "eisenhowersquadrant": "https://eisenhowersquadrant.herokuapp.com/", 
+    "depechehouse": "https://depechehouse.herokuapp.com/", 
+    "depechecodes": "", 
+    "mileagecalculator": ""
+    }
 
-project_images = {"eisenhowersquadrant": "", "depechehouse": "", "depechecodes": "", "mileagecalculator": ""}
+project_images = {
+    "eisenhowersquadrant": "background-image: url(../static/images/quadrants.jpg)", 
+    "depechehouse": "background-image: url(../static/images/house.jpg)", 
+    "depechecodes": "background-image: url(../static/images/pawel-czerwinski-ISgDx2aOAB8-unsplash.jpg)", 
+    "mileagecalculator": "background-image: url(../static/images/tesla.jpg)"
+    }
+
+
+
 
